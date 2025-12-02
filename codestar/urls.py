ca6f215b17a31f06f2fe0f83002637fe499b6da5
Greggy1234 <gregoreid@googlemail.com>
2025-12-02 20:00:34 +0000

    Create about app for blog

--- codestar/urls.py
+++ codestar/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('about/', include('about.urls'), name="about-url"),
     path('admin/', admin.site.urls),
     path('summernote', include('django_summernote.urls')),
-    path('', include("blog.urls"), name="blog-urls"),
+    path('', include("blog.urls"), name="blog-urls"),    
 ]
